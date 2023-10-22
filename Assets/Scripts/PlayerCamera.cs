40be497739bac9d3d80acea50790fc3888f52498
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-10-22 16:34:58 +0900

    change directory

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target; // Link to the character's Transform.
+    public float distance = 5.0f; // Distance between the character and camera.
+    public float height = 2.0f; // Adjust the height of the camera.
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    public float xSpeed = 120.0f; // Mouse rotation speed.
+    public float ySpeed = 120.0f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+    }
+
+    void LateUpdate()
+    {
+        if (!target)
+            return;
+
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+        // Clamp the vertical angle to prevent the camera from flipping over.
+        y = ClampAngle(y, -50, 80);
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position = rotation * new Vector3(0.0f, height, -distance) + target.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))] // component 자동으로 추가 
+public class PlayerMove : MonoBehaviour
+{
+    Vector3 destPos;
+    Vector3 dir;
+    Quaternion lookTarget;
+
+    bool move = false;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            // 메인 카메라를 통해 마우스 클릭한 곳의 ray 정보를 가져옴
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // ray와 닿은 물체가 있는지 검사
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                print(hit.transform.name);
+
+                // hit.point 는 마우스 클릭한 곳의 월드 좌표.
+                // 이 예제에서 캐릭터의 y 값(높이) 은 변하지 않기 때문에 
+                // 아래와 같이 목표위치를 재설정합니다.
+                destPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+
+                // 현재 위치와 목표 위치의 방향 벡터
+                dir = destPos - transform.position;
+
+                // 바라 보아야 할 곳의 Quaternion
+                lookTarget = Quaternion.LookRotation(dir);
+                move = true;
+            }
+        }
+
+        Move();
+    }
+
+    void Move()
+    {
+        if (move)
+        {
+            // 이동할 방향으로 Time.deltaTime * 2f 의 속도로 움직임.
+            transform.position += dir.normalized * Time.deltaTime * 2f;
+            // 현재 방향에서 움직여야할 방향으로 부드럽게 회전
+            transform.rotation = Quaternion.Lerp(transform.rotation, lookTarget, 0.25f);
+
+            // 캐릭터의 위치와 목표 위치의 거리가 0.05f 보다 큰 동안만 이동
+            move = (transform.position - destPos).magnitude > 0.05f;
+        }
+    }
+}
